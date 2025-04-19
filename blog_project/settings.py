4845a6822e42e255a9efcbb79737d1a6de70b985
Anthony Rojas <anthony.rojas099@gmail.com>
2025-04-19 01:48:26 -0700

    Ready for Heroku deployment

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os 
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['mets-blog.herokuapp.com']
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
